a4140eeda723a88a750473b582a82d4e7a6b4edc
Björn-Egil Dahlberg <egil@hapida.se>
2012-11-25 03:46:55 +0100

    Strengten colorscheme due to tests

--- src/egd_colorscheme.erl
+++ src/egd_colorscheme.erl
@@ -36,7 +36,7 @@ select(I, Hm, S, L, A) -> egd:color(hsl2rgb({I*Hm rem 360, S, L, A})).
 %% S, saturation has the range of [0,1]
 %% L, lightness has the range of [0,1]
 
--define(float_error, 0.0000000000000001).
+-define(float_error, 0.000000000001).
 
 hsl2rgb({H,S,L}) -> hsl2rgb({H,S,L,255});
 hsl2rgb({H,S,L,A}) ->
@@ -63,26 +63,36 @@ hsl2rgb({H,S,L,A}) ->
     end, Cts),
     {trunc(R*255),trunc(G*255),trunc(B*255),A}.
 
+byte2float(B) -> envelope(B/255, 0, 1).
+
+envelope(V, Min, Max) when V >= Min andalso V =< Max -> V;
+envelope(V, Min, Max) when V < Min andalso V < Max -> Min;
+envelope(V, Min, Max) when V > Min andalso V > Max -> Max.
+
 rgb2hsl({R,G,B}) -> rgb2hsl({R,G,B,255});
 rgb2hsl({R,G,B,A}) ->
-    Rf  = R/255,
-    Gf  = G/255,
-    Bf  = B/255,
+    Rf  = byte2float(R),
+    Gf  = byte2float(G),
+    Bf  = byte2float(B),
+
     Max = lists:max([Rf,Gf,Bf]),
     Min = lists:min([Rf,Gf,Bf]),
     H   = if
 	    abs(Max - Min) < ?float_error ->
-		0;
+		0.0;
 	    abs(Max - Rf)  < ?float_error ->
 		D  = 60 * (Gf - Bf)/(Max - Min),
 		Dt = trunc(D),
-	        Dt rem 360;
+		case {((Dt + 360) rem 360), (D - Dt)} of
+		    {0, Frac} when Frac < 0 -> 360.0 + Frac;
+		    {Degs, Frac} -> Degs + Frac
+		end;
 	    abs(Max - Gf) < ?float_error ->
 		60 * (Bf - Rf)/(Max - Min) + 120;
 	    abs(Max - Bf) < ?float_error ->
 		60 * (Rf - Gf)/(Max - Min) + 240;
 	    true -> 
-	    0
+		0.0
 	end,
     L   = (Max + Min)/2,
     S   = if
@@ -93,7 +103,4 @@ rgb2hsl({R,G,B,A}) ->
 	    true ->
 		(Max - Min)/(Max + Min)
 	end,
-    {H, S, L, A}.
-
-
-
+    {H, envelope(S, 0.0, 1.0), envelope(L, 0.0, 1.0), A}.
